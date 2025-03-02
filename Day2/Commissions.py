a1777bd36f303fdbdc11cac5fad80ebed4c7054c
Klee Collage <klee.collage@gmail.com>
2025-03-01 19:41:28 -0600

    Operators and round() function

--- Day2/Commissions.py
+++ Day2/Commissions.py
@@ -1,5 +1,5 @@
 name = input("What is your name? ")
-sells = int(input("How many sells do you have? "))
-sells = float(sells)
+sells = float(input("How many sells do you have? "))
 commission = round(sells * 0.13, 2)
+
 print(f"{name} sells ${sells} commission per month: ${commission}")
