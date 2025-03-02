8297e6f6c0f75ad42fd65cc2b5d59789836d2d64
Klee Collage <klee.collage@gmail.com>
2025-03-01 18:20:41 -0600

    Hello World

---
+++ First_Project.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print(100 + 50)
+print("My name is \"John Doe\"")
+print("\tThis is one line \nThis is another line")
+print("This \\ is a reverse diagonal")
+
+print(
+    "Your username is: " +
+    input("Enter your favorite color: "),
+    input("Enter your last meal: ")
+)
+
