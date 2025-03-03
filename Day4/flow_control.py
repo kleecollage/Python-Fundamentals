acc13a8efd8869620752dfdd76872ad8fd1a4192
Klee Collage <klee.collage@gmail.com>
2025-03-03 15:05:55 -0600

    Flow Control

---
+++ Day4/flow_control.py
@@ -0,0 +1,41 @@
+x = True
+if x:
+    print("Correct")
+
+if 10 > 9:
+    print("Correct")
+
+if 5 == 2:
+    print("Correct")
+else:
+    print("Incorrect")
+
+
+mascot = 'dog'
+if mascot == 'cat':
+    print("You have a cat")
+else:
+    print("I dont know that mascot owo")
+
+if mascot == 'cat':
+    print("You have a cat")
+elif mascot == 'fish':
+    print("You have a dog")
+elif mascot == 'dog':
+    print("You have a dog")
+else:
+    print("I dont know that animal owo")
+
+age = 16
+grade = 9
+if age >= 18:
+    print("You are a minor")
+    if grade >= 7:
+        print("Approved")
+    else:
+        print("Not approved")
+else:
+    print("You can vote")
+
+
+
