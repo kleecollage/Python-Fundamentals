388efa73abead2a30b667ecbdc89d1bb60ae6658
Klee Collage <klee.collage@gmail.com>
2025-03-03 16:03:43 -0600

    Range

---
+++ Day4/range.py
@@ -0,0 +1,17 @@
+for num in range(10,30,3):
+    print(num)
+
+my_list = list(range(1,101))
+print(my_list)
+
+my_list = list(range(2500,2586))
+print(my_list)
+
+my_list = list(range(3,301,3))
+print(my_list)
+
+square_sum = 0
+for nums in range(16):
+    square_sum += nums**2
+
+print(square_sum)
