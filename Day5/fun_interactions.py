e121ed4651e0e255cc136857e28439793dadce3b
Klee Collage <klee.collage@gmail.com>
2025-03-04 15:17:10 -0600

    Interaction Between Functions and Unpacked Tuples

--- Day5/fun_interactions.py
+++ Day5/fun_interactions.py
@@ -58,3 +58,21 @@ def promedio(ylist):
     return sum(ylist) / len(ylist)
 
 print(promedio(reducir_lista(lista_numeros)))
+
+## HEADS OR TAILS ##
+import random
+
+options = ["Cara", "Cruz"]
+lista_numeros = [1, 2, 3]
+
+def lanzar_moneda():
+    return random.choice(options)
+
+def probar_suerte(result, nlist):
+    if result == 'Cara':
+        print("La lista se autodestruirá")
+        return []
+    print("La lista fue salvada")
+    return lista_numeros
+
+print(probar_suerte(lanzar_moneda(), lista_numeros))
