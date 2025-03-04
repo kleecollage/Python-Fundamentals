480ec4c34c8a125ad6ad8878eaaa2389d5ee7529
Klee Collage <klee.collage@gmail.com>
2025-03-04 13:41:10 -0600

    Functions

---
+++ Day5/functions.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello", name)
+    '''
+    Hello
+    This function prints out the greeting
+    '''
+
+greeting("John")
+greeting("Jane")
+
---
+++ Day5/return.py
@@ -0,0 +1,12 @@
+def multiply(a, b):
+    total = a*b
+    return total
+
+result = multiply(5, 2)
+print(result)
+
+
+
+def invertir_palabra(word):
+    return word[::-1].upper()
+palabra = "Python"
