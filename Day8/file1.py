b5c77adeaa0e706d59b05cf6a9be6b88ccee4ddc
Klee Collage <klee.collage@gmail.com>
2025-03-13 14:27:28 -0600

    Pylint

---
+++ Day8/file1.py
@@ -0,0 +1,9 @@
+"""
+This is a module to print something
+"""
+
+def my_function():
+    number1 = 500
+    print(number1)
+
+my_function()
---
+++ Day8/pylint_practice.py
@@ -0,0 +1,11 @@
+"""
+This is a test for pylint
+"""
+def my_sum(num1, num2):
+    """
+    this function sums two numbers
+    """
+    return num1+num2
+
+GET_SUM = my_sum(5,7)
+print(GET_SUM)
