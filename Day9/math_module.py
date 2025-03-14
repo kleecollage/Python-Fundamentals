39c4c48f1658a715291e4e069398bade46b0f55d
Klee Collage <klee.collage@gmail.com>
2025-03-14 14:25:29 -0600

    Math module

---
+++ Day9/math_module.py
@@ -0,0 +1,17 @@
+import math
+
+
+result = math.floor(89.665)
+print(result)
+
+result = math.ceil(89.665)
+print(result)
+
+result = math.pi
+print(result)
+
+result = math.log(16, 2)
+print(result)
+
+result = math.tan(math.pi/2)
+print(result)
