d48a39728b16ad96a6f7116b1ac11d69203ecdd7
Klee Collage <klee.collage@gmail.com>
2025-03-18 16:20:37 -0600

    feat(registerView): Implement register

--- Day16/src/WebProject/base/urls.py
+++ Day16/src/WebProject/base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ListTasks, DetailTask, CreateTask, UpdateTask, DeleteTask, Login
+from .views import ListTasks, DetailTask, CreateTask, UpdateTask, DeleteTask, Login, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', ListTasks.as_view(), name='tasks'),
     path('task/<int:pk>', DetailTask.as_view(), name='details'),
     path('create-task/', CreateTask.as_view(), name='create'),
