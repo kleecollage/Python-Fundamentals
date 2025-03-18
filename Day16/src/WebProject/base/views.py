f2c7a9eb6165208a6066f7496466580f57e4e1f4
Klee Collage <klee.collage@gmail.com>
2025-03-18 16:11:50 -0600

    feat(restrict access): Tasks details restricted to owners

--- Day16/src/WebProject/base/views.py
+++ Day16/src/WebProject/base/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import Http404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.http import HttpResponse
 from base.models import Task
 
 # Create your views here.
@@ -24,20 +26,42 @@ class ListTasks(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'Tasks'
     template_name = 'base/task_list.html'
+    def get_context_data(self, **kwarg):
+        context = super().get_context_data(**kwarg)
+        context['Tasks'] = context['Tasks'].filter(user=self.request.user)
+        context['count'] = context['Tasks'].filter(completed=False).count()
+        return context
 
 class DetailTask(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'base/task_detail.html'
+    # Verify if user have permission to see the object
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            obj = self.get_object()
+            if obj.user != self.request.user:
+                return redirect('tasks')
+        except Http404:
+            return redirect('tasks')
+        return super().dispatch(request, *args, **kwargs)
+    # Filter by task that belongs to the logged user
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
 class CreateTask(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'completed']
     success_url = reverse_lazy('tasks')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateTask, self).form_valid(form)
 
 class UpdateTask(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    # fields = '__all__'
+    fields = ['title', 'description', 'completed']
     success_url = reverse_lazy('tasks')
 
 class DeleteTask(LoginRequiredMixin, DeleteView):
