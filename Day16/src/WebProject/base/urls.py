fe69c943de5c1029312b1dff7bb392a0738fa88b
Klee Collage <klee.collage@gmail.com>
2025-03-18 14:38:01 -0600

    feat(delete,update views): Implement views to remove and update tasks

--- Day16/src/WebProject/base/urls.py
+++ Day16/src/WebProject/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ListTasks, DetailTask, CreateTask
+from .views import ListTasks, DetailTask, CreateTask, UpdateTask, DeleteTask
 
 urlpatterns = [
     path('', ListTasks.as_view(), name='tasks'),
     path('task/<int:pk>', DetailTask.as_view(), name='details'),
     path('create-task/', CreateTask.as_view(), name='create'),
+    path('update-task/<int:pk>', UpdateTask.as_view(), name='update'),
+    path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete'),
 ]
--- Day16/src/WebProject/base/views.py
+++ Day16/src/WebProject/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from base.models import Task
 
@@ -26,6 +26,17 @@ class CreateTask(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class UpdateTask(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'Task'
+    success_url = reverse_lazy('tasks')
+    template_name = 'base/task_confirm_delete.html'
 
 
 
