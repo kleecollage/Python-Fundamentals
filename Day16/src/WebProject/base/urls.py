d0060f61b38d6d75b9dfdd24fab8998816e09164
Klee Collage <klee.collage@gmail.com>
2025-03-18 14:24:26 -0600

    feat(detail, form): Implemetn view for details and create new tasks

--- Day16/src/WebProject/base/urls.py
+++ Day16/src/WebProject/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-from .views import ListTasks
+from .views import ListTasks, DetailTask, CreateTask
 
 urlpatterns = [
     path('', ListTasks.as_view(), name='tasks'),
+    path('task/<int:pk>', DetailTask.as_view(), name='details'),
+    path('create-task/', CreateTask.as_view(), name='create'),
 ]
--- Day16/src/WebProject/base/views.py
+++ Day16/src/WebProject/base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from base.models import Task
-
 
 # Create your views here.
 
@@ -13,3 +15,25 @@ from base.models import Task
 class ListTasks(ListView):
     model = Task
     context_object_name = 'Tasks'
+    template_name = 'base/task_list.html'
+
+class DetailTask(DetailView):
+    model = Task
+    context_object_name = 'Task'
+    template_name = 'base/task_detail.html'
+
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+
+
+
+
+
+
+
+
+
